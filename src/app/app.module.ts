b43129d5c5efb9a9d2363e5d54c48aecc6364c41
Antonio Ribeiro <antoniodrigues@hotmail.com>
2022-06-24 08:08:33 -0300

    Implementação dos primeiros cards estáticos utilizando diretiva NgFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,15 +7,9 @@ import { AppComponent } from './app.component';
 import { MainPokeComponent } from './componentes/main-poke/main-poke.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainPokeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MainPokeComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/mock.pokes.ts
@@ -0,0 +1,28 @@
+import { Poke } from './../assets/modules/poke.model';
+
+export const POKES: Poke[] = [
+  {
+    id: 1,
+    name: 'Charmander',
+    link: 'www.Charmander.com',
+    image: 'Charmander.img',
+  },
+  {
+    id: 2,
+    name: 'Bubassar',
+    link: 'www.Bubassar.com',
+    image: 'Bubassar.img',
+  },
+  {
+    id: 3,
+    name: 'Squirtle',
+    link: 'www.Squirtle.com',
+    image: 'Squirtle.img',
+  },
+  {
+    id: 4,
+    name: 'Pikachu',
+    link: 'www.Pikachu.com',
+    image: 'Pikachu.img',
+  },
+];
